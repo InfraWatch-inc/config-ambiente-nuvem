a5a3cd0e0262cbf3afe934fd7b5750c192b15a93
RanierDalton <ranier.couto@sptech.school>
2025-05-24 16:15:06 -0300

    refactor: Refatorando script de configuração d instancia de nossa Infra

--- scriptBash/configInstancia.sh
+++ scriptBash/configInstancia.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
 # Comados ao abrir instancia:
-# ssh -i token.pem ubuntu@dns/ip - conectar cm a instancia (local)
-# mkdir producao/  -  criar pasta para guardar o zip q vamos enviar (nuvem)
-# scp -i token.pem arquivo.zip ubuntu@dns/ip:producao/ - enviar o zip pra pasta (local)
-# cd producao/ - entrar na pasta q criamos (nuvem)
-# sudo apt-get install unzip - instalar lib para deszipar arquivo (nuvem)
-# unzip arquivo.zip - deszipar o arquivo (nuvem)
-# chmod 777 Scripts/configInstancia.sh - dar permissão ao script p rodar (nuvem)
-# ./Scripts/configInstancia.sh - rodar o script e rezar muito pra funfar (nuvem)
+# git clone https://github.com/InfraWatch-inc/config-ambiente-nuvem.git
 
 # os q's são de quiet (silencio) para mostrar menos texto (por mais q monstre uma porrada ainda assim)
 # e os y's são para não ficar fazendo perguntas no meio dos comandos
-
-echo -e "\033[41;1;37m Indo ao local correto... \033[0m"
-cd ~/infraWatch
 
 # Atualizar a máquina
 echo -e "\033[41;1;37m Atualizando Sistema... \033[0m"
 sudo apt update -qq -y
 sudo apt upgrade -qq -y
-
-# Instalar Node.js 
-echo -e "\033[41;1;37m  Instalando NodeJS... \033[0m" # formatacao de texto vermelho pra deixar destacado
-sudo apt install -qq -y nodejs npm
 
 # instalando Docker
 echo -e "\033[41;1;37m Instalando Docker... \033[0m"
@@ -35,82 +21,6 @@ exit
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Baixando imagen do Mysql
-echo -e "\033[41;1;37m Baixando imagen do Mysql... \033[0m"
-sudo docker pull mysql:8.2 
-
-# Criando conteiner 
-echo -e "\033[41;1;37m Criando conteiner e iniciando... \033[0m"
-sudo docker run -d -p 3000:3306 --name db -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:8.2 
-
-# Clonando o repositório database 
-echo -e "\033[41;1;37m Clonando o repositório databsae... \033[0m"
-git clone https://github.com/InfraWatch-inc/database.git
-
-# Entrando no conteiner.
-echo -e "\033[41;1;37m Enviando o script sql para o container... \033[0m"
-sudo docker cp database/script.sql db:/tmp/script.sql
-
-# Executando o script sql
-echo -e "\033[41;1;37m Executando o script do banco de dados, criando e estruturando BD... \033[0m"
-docker exec -i db mysql -u root -p "urubu100" < /tmp/arquivo.sql
-
-
-# Salvar as regras de iptables
-sudo apt-get -y update
-sudo apt-get install -y iptables-persistent
-
-sudo netfilter-persistent -y save
-sudo netfilter-persistent -y reload
-
-# Acessar o docker
-echo -e "\033[41;1;37m Entrando no conteiner... \033[0m"
-sudo docker -it bd bash
-sudo mysql -u infrawatch -p "urubu100"
-
-# 
-
-# Liberar a porta 3306 para qualquer IP
-# echo -e "\033[41;1;37m Liberar 3306 do Servidor de Banco... \033[0m"
-# sudo iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
-
-
-# # configurando MYSQL
-# echo -e "\033[41;1;37m Criando e estruturando BD infrawatch... \033[0m"
-# sudo mysql < ~/InfraWatch-inc/database/script.sql
-
-# Criar usuários MYSQL
-echo -e "\033[41;1;37m Criando Usuários do Banco... \033[0m"
-sudo mysql -e"CREATE USER 'infra_root'@'%' IDENTIFIED BY 'Urubu100#';"
-sudo mysql -e"GRANT ALL PRIVILEGES ON infrawatch.* TO 'infra_root'@'%';"
-sudo mysql -e"FLUSH PRIVILEGES;"
-
-sudo mysql -e"CREATE USER 'insert_user'@'%' IDENTIFIED BY 'Urubu100#';"
-sudo mysql -e"GRANT INSERT ON infrawatch.* TO 'insert_user'@'%';"
-sudo mysql -e"FLUSH PRIVILEGES;"
-
-sudo mysql -e"CREATE USER 'select_user'@'%' IDENTIFIED BY 'Urubu100#';"
-sudo mysql -e"GRANT SELECT ON infrawatch.* TO 'select_user'@'%';"
-sudo mysql -e"FLUSH PRIVILEGES;"
-
-sudo mysql -e"CREATE USER 'update_user'@'%' IDENTIFIED BY 'Urubu100#';"
-sudo mysql -e"GRANT UPDATE ON infrawatch.* TO 'update_user'@'%';"
-sudo mysql -e"FLUSH PRIVILEGES;"
-
-sudo mysql -e"CREATE USER 'delete_user'@'%' IDENTIFIED BY 'Urubu100#';"
-sudo mysql -e"GRANT DELETE ON infrawatch.* TO 'delete_user'@'%';"
-sudo mysql -e"FLUSH PRIVILEGES;"
-
-# Saindo do conteiner
-echo -e "\033[41;1;37m Saindo do conteiner, para execução do node... \033[0m"
-exit
-
-# Clonando aplicação web
-echo -e "\033[41;1;37m Clonando web-data-viz... \033[0m"
-git clone --quiet https://github.com/InfraWatch-inc/web-data-viz.git
-
-# configurar e rodar projeto node
-echo -e "\033[41;1;37m Configurando e inicializando web-data-viz... \033[0m"
-npm --prefix ./web-data-viz install
-npm --prefix ./web-data-viz audit fix
-npm --prefix ./web-data-viz start
+# Iniciar docker compose
+echo -e "\033[41;1;37m Iniciando docker compose... \033[0m"
+sudo docker compose up -d
